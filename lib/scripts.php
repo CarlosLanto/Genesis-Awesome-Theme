8ae4244b9290395a70ebc4e001354f2199df2a3f
Carlos Lanto <carloslantog@gmail.com>
2014-09-09 12:47:34 -0500

    Add Wow.js & Update FontAwesome

--- lib/scripts.php
+++ lib/scripts.php
@@ -1,8 +1,9 @@
 <?php 
-
 /**
  * Script Register and Enqueue
  */
+
+if ( ! defined( 'ABSPATH' ) ) exit( 'Cheatin&#8217; uh?' ); // Exit if accessed directly
 
 /*
 Scripts
@@ -18,10 +19,25 @@ function genesis_sample_google_fonts() {
 add_action('wp_enqueue_scripts', 'gat_custom_scripts' );
 function gat_custom_scripts(){
 		//* CSS's
-	wp_enqueue_style( 'gat-font-awesome', get_bloginfo( 'stylesheet_directory' ) . '/lib/css/font-awesome.min.css', array(), '4.1.0' );
+	wp_enqueue_style( 'gat-font-awesome', get_bloginfo( 'stylesheet_directory' ) . '/lib/css/font-awesome.min.css', array(), '4.2.0' );
+	wp_enqueue_style( 'animate', get_bloginfo( 'stylesheet_directory' ). '/lib/css/animate.min.css');
 	wp_enqueue_style( 'bootstrap', get_bloginfo( 'stylesheet_directory' ). '/lib/css/bootstrap.css');
 	//wp_enqueue_style( 'bootstrap-theme', get_bloginfo( 'stylesheet_directory' ). '/lib/css/bootstrap-theme.min.css' );
 		//* JS's
-	wp_enqueue_script( 'bootstrap-js', get_bloginfo( 'stylesheet_directory'). '/lib/js/bootstrap.min.js');
-	wp_enqueue_script( 'jquery-2.1.1', get_bloginfo( 'stylesheet_directory'). '/lib/js/jquery-2.1.1.min.js');
+	wp_enqueue_script( 'jquery-2.1.1', get_bloginfo( 'stylesheet_directory') . '/lib/js/jquery-2.1.1.min.js');
+	wp_enqueue_script( 'bootstrap-js', get_bloginfo( 'stylesheet_directory') . '/lib/js/bootstrap.min.js', array( 'jquery' ) );
+	wp_enqueue_script( 'wow', get_bloginfo( 'stylesheet_directory') . '/lib/js/wow.min.js', array(), '', true );
 }
+
+//* Enqueue script in Footer to activate WOW.js
+add_action('wp_enqueue_scripts', 'sb_init_wow_in__footer');
+function sb_init_wow_in__footer() {
+	add_action( 'print_footer_scripts', 'wow_init' );
+}
+ 
+//* Add JavaScript before </body>
+function wow_init() { ?>
+	<script>
+		new WOW().init();
+	</script>
+<?php }
